63585b90108270dff799bbb5b1f072b695722415
Sumaia Damlakhi <somaia.damlakhi@startappz.com>
2024-08-18 17:28:30 -0400

    Create Splash Screen design and animation

--- app/src/main/java/com/blueray/bookreader/MainActivity.kt
+++ app/src/main/java/com/blueray/bookreader/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.blueray.bookreader.navigation.ReaderNavigation
 import com.blueray.bookreader.ui.theme.BookReaderTheme
 import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
@@ -29,10 +35,12 @@ class MainActivity : ComponentActivity() {
             BookReaderTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 46.dp),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ReaderApp()
                 }
             }
         }
@@ -41,18 +49,19 @@ class MainActivity : ComponentActivity() {
 
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BookReaderTheme {
-        Greeting("Android")
+fun ReaderApp(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize(),
+
+        ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ReaderNavigation()
+        }
     }
 }
