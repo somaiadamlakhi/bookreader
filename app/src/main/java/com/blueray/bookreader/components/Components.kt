ae19ffd40463dd5947b8d4945659454207a53e81
Sumaia Damlakhi <somaia.damlakhi@startappz.com>
2024-08-22 22:04:08 -0400

    create email input field

--- app/src/main/java/com/blueray/bookreader/components/Components.kt
+++ app/src/main/java/com/blueray/bookreader/components/Components.kt
@@ -1,13 +1,22 @@
 package com.blueray.bookreader.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.blueray.bookreader.R
 
 
@@ -20,5 +29,41 @@ fun ReaderLogo(modifier: Modifier = Modifier) {
         color = Color.Red.copy(
             alpha = 0.5f
         )
+    )
+}
+
+
+
+@Composable
+fun InputFiled(
+    modifier: Modifier = Modifier,
+    valueState: MutableState<String>,
+    isSingleLine: Boolean = true,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Next,
+    onAction: KeyboardActions = KeyboardActions.Default,
+    labelID: String,
+    enabled: Boolean = true
+) {
+
+    OutlinedTextField(
+        value = valueState.value,
+        onValueChange = {
+            valueState.value = it
+        },
+        label = {
+            Text(text = labelID)
+        },
+        singleLine = isSingleLine,
+        textStyle = TextStyle(fontSize = 18.sp, color = MaterialTheme.colorScheme.onBackground),
+        modifier = modifier
+            .padding(bottom = 10.dp, start = 10.dp, end = 10.dp)
+            .fillMaxWidth(),
+        enabled = enabled,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType,
+            imeAction = imeAction,
+        ),
+        keyboardActions = onAction
     )
 }
--- app/src/main/java/com/blueray/bookreader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/blueray/bookreader/screens/login/ReaderLoginScreen.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -17,10 +19,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.platform.SoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.blueray.bookreader.components.InputFiled
 import com.blueray.bookreader.components.ReaderLogo
 
 @Composable
@@ -48,7 +52,6 @@ fun UserForm() {
     }
 
 
-
     val modifier = Modifier
         .height(250.dp)
         .background(MaterialTheme.colorScheme.background)
@@ -59,7 +62,37 @@ fun UserForm() {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        EmailInput(
+            emailState = email,
+            enabled = true,
+            onAction = KeyboardActions {
+                passwordFocusRequest.requestFocus()
+            }
+        )
     }
+
+}
+
+
+@Composable
+fun EmailInput(
+    modifier: Modifier = Modifier,
+    emailState: MutableState<String>,
+    labelID: String = "email",
+    enabled: Boolean = true,
+    imeAction: ImeAction = ImeAction.Next,
+    onAction: KeyboardActions = KeyboardActions.Default
+) {
+    InputFiled(
+        modifier = modifier,
+        valueState = emailState,
+        labelID = labelID,
+        enabled = enabled,
+        keyboardType = KeyboardType.Email,
+        imeAction = imeAction,
+        onAction = onAction
+
+
+    )
 
 }
