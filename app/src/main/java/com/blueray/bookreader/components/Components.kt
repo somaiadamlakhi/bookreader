9c5bb9bfaaeae3f257a50d1432500cc3d40be5ab
Sumaia Damlakhi <somaia.damlakhi@startappz.com>
2024-08-23 20:56:57 -0400

    adding password filed refactor the code

--- app/src/main/java/com/blueray/bookreader/components/Components.kt
+++ app/src/main/java/com/blueray/bookreader/components/Components.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -15,6 +18,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.blueray.bookreader.R
@@ -66,4 +71,78 @@ fun InputFiled(
         ),
         keyboardActions = onAction
     )
+}
+
+
+@Composable
+fun EmailInput(
+    modifier: Modifier = Modifier,
+    emailState: MutableState<String>,
+    labelID: String = "email",
+    enabled: Boolean = true,
+    imeAction: ImeAction = ImeAction.Next,
+    onAction: KeyboardActions = KeyboardActions.Default
+) {
+    InputFiled(
+        modifier = modifier,
+        valueState = emailState,
+        labelID = labelID,
+        enabled = enabled,
+        keyboardType = KeyboardType.Email,
+        imeAction = imeAction,
+        onAction = onAction
+
+
+    )
+
+}
+
+@Composable
+fun PasswordInput(
+    modifier: Modifier = Modifier,
+    passwordState: MutableState<String>,
+    enabled: Boolean,
+    passwordVisibility: MutableState<Boolean>,
+    onAction: KeyboardActions = KeyboardActions.Default,
+    imeAction: ImeAction = ImeAction.Done,
+    labelID: String = "Password",
+
+    ) {
+
+    val visualTransformation = if (passwordVisibility.value) VisualTransformation.None
+    else PasswordVisualTransformation()
+
+    OutlinedTextField(value = passwordState.value,
+        onValueChange = {
+            passwordState.value = it
+        },
+        label = { Text(text = labelID) },
+        singleLine = true,
+        textStyle = TextStyle(
+            fontSize = 18.sp, color = MaterialTheme.colorScheme.onBackground
+        ),
+        modifier = modifier
+            .padding(bottom = 10.dp, start = 10.dp, end = 10.dp)
+            .fillMaxWidth(),
+        enabled = enabled,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password, imeAction = imeAction
+        ),
+        visualTransformation = visualTransformation,
+        trailingIcon = {
+            PasswordVisibility(passwordVisibility = passwordVisibility)
+        }
+    )
+
+}
+
+@Composable
+fun PasswordVisibility(passwordVisibility: MutableState<Boolean>) {
+    val visible = passwordVisibility.value
+    IconButton(onClick = {
+        passwordVisibility.value = !visible
+    }) {
+
+        Icons.Default.Close
+    }
 }
--- app/src/main/java/com/blueray/bookreader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/blueray/bookreader/screens/login/ReaderLoginScreen.kt
@@ -1,5 +1,6 @@
 package com.blueray.bookreader.screens.login
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,20 +12,19 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.blueray.bookreader.components.InputFiled
+import com.blueray.bookreader.components.EmailInput
+import com.blueray.bookreader.components.PasswordInput
 import com.blueray.bookreader.components.ReaderLogo
 
 @Composable
@@ -35,19 +35,29 @@ fun ReaderLoginScreen(navController: NavHostController) {
             verticalArrangement = Arrangement.Top
         ) {
             ReaderLogo()
+            UserForm(
+                loading = false,
+                isCreateAccount = false
+            ) { email, password ->
+                Log.d("Reader Login Screen", "@email $email @Password $password")
+            }
         }
     }
 }
 
 @Preview
 @Composable
-fun UserForm() {
+fun UserForm(
+    loading: Boolean = false,
+    isCreateAccount: Boolean = false,
+    onDone: (String, String) -> Unit = { email, password -> }
+) {
     val email = rememberSaveable { mutableStateOf("") }
     val password = rememberSaveable { mutableStateOf("") }
     val passwordVisibility = rememberSaveable { mutableStateOf(false) }
     val passwordFocusRequest = FocusRequester.Default
     val keyboardController = LocalSoftwareKeyboardController.current
-    val validRemember = remember(email, password) {
+    val valid = remember(email, password) {
         email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
     }
 
@@ -59,40 +69,21 @@ fun UserForm() {
 
 
     Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        EmailInput(
-            emailState = email,
-            enabled = true,
+        EmailInput(emailState = email, enabled = !loading, onAction = KeyboardActions {
+            passwordFocusRequest.requestFocus()
+        })
+
+        PasswordInput(modifier = Modifier.focusRequester(passwordFocusRequest),
+            passwordState = password,
+            enabled = !loading,
+            passwordVisibility = passwordVisibility,
             onAction = KeyboardActions {
-                passwordFocusRequest.requestFocus()
-            }
-        )
+                if (!valid) return@KeyboardActions
+                onDone(email.value.trim(), password.value.trim())
+            })
     }
 
 }
 
-
-@Composable
-fun EmailInput(
-    modifier: Modifier = Modifier,
-    emailState: MutableState<String>,
-    labelID: String = "email",
-    enabled: Boolean = true,
-    imeAction: ImeAction = ImeAction.Next,
-    onAction: KeyboardActions = KeyboardActions.Default
-) {
-    InputFiled(
-        modifier = modifier,
-        valueState = emailState,
-        labelID = labelID,
-        enabled = enabled,
-        keyboardType = KeyboardType.Email,
-        imeAction = imeAction,
-        onAction = onAction
-
-
-    )
-
-}
