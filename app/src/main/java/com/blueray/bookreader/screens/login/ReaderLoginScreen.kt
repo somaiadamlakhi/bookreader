3e4370cddcbf4b3bdc59baad0d0891cd3de55293
Sumaia Damlakhi <somaia.damlakhi@startappz.com>
2024-08-27 23:47:06 -0400

    adding submit button

--- app/src/main/java/com/blueray/bookreader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/blueray/bookreader/screens/login/ReaderLoginScreen.kt
@@ -5,12 +5,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -20,9 +27,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.blueray.bookreader.R
 import com.blueray.bookreader.components.EmailInput
 import com.blueray.bookreader.components.PasswordInput
 import com.blueray.bookreader.components.ReaderLogo
@@ -57,10 +66,9 @@ fun UserForm(
     val passwordVisibility = rememberSaveable { mutableStateOf(false) }
     val passwordFocusRequest = FocusRequester.Default
     val keyboardController = LocalSoftwareKeyboardController.current
-    val valid = remember(email, password) {
+    val valid = remember(email.value, password.value) {
         email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
     }
-
 
     val modifier = Modifier
         .height(250.dp)
@@ -83,6 +91,36 @@ fun UserForm(
                 if (!valid) return@KeyboardActions
                 onDone(email.value.trim(), password.value.trim())
             })
+
+        SubmitButton(
+            textId = if (isCreateAccount) stringResource(id = R.string.create_account)
+            else stringResource(id = R.string.login),
+            loading = loading,
+            validInputs = valid
+        ) {
+            onDone(email.value.trim(), password.value.trim())
+        }
+    }
+
+}
+
+@Composable
+fun SubmitButton(
+    textId: String,
+    loading: Boolean,
+    validInputs: Boolean,
+    onclick: () -> Unit
+) {
+    Button(
+        onClick = onclick,
+        modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth(),
+        enabled = !loading && validInputs,
+        shape = CircleShape
+    ) {
+        if (loading) CircularProgressIndicator(modifier = Modifier.size(25.dp))
+        else Text(text = textId, modifier = Modifier.padding(5.dp))
     }
 
 }
